9c1b45ff5d50f610584bc3a07761093545e051f3
emmanuel <emmanuelareizaperez2@gmail.com>
2022-11-23 01:37:19 -0500

    Se Agrego el Componente [Saludo] y se le dio estilo

---
+++ src/componentes/Saludo.js
@@ -0,0 +1,11 @@
+import './estilos/estilos.css';
+export  function Saludar({nombre})
+{
+    return <div  className="Saludo">
+        
+        {<div><p>perro</p></div>}
+        {<div><p>perro</p></div>}
+        
+        </div>;
+}
+
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
+import {Saludar} from './componentes/Saludo';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //render.
 root.render(
 
+  <Saludar nombre="popo"/>
+  
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
