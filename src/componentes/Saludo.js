553994f5c8a181deb513a5d35bba577da1ffb3da
emmanuel <emmanuelareizaperez2@gmail.com>
2022-11-23 18:10:36 -0500

    <Saludo>[Input,Button] Se les asignó estado y se hace uso de un efecto para monitorear dicho estado

--- src/componentes/Saludo.js
+++ src/componentes/Saludo.js
@@ -1,11 +1,33 @@
+import { useState ,useEffect} from 'react';
 import './estilos/estilos.css';
-export  function Saludar({nombre})
+
+const Input =()=>
 {
-    return <div  className="Saludo">
-        
-        {<div><p>perro</p></div>}
-        {<div><p>perro</p></div>}
-        
-        </div>;
+
+
+
+    const [nombre,definirNombre]=useState("ValorPrincipalVacío");
+    useEffect(()=>
+    {
+        console.log("Cambio")
+    },
+    [nombre])
+
+
+    return <div>
+    <input 
+    name="popo" placeholder="Inserta tú nombre" 
+    onChange={(e)=>definirNombre(e.target.value)
+    }/>{} </div>;
+}
+
+export  function Saludar()
+{
+
+    return<>
+        <Input/>
+        <button onClick={()=>{console.log("pop")}} >Eso</button>
+
+    </>;
 }
 
